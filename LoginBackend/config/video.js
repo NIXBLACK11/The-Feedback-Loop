20cadbcec9ee5ecfe593451314c85256e9c9f821
Siddharth Singh Rana <91743459+NIXBLACK11@users.noreply.github.com>
2024-03-16 18:16:50 +0530

    Delete LoginBackend directory

--- LoginBackend/config/video.js
+++
@@ -1,9 +0,0 @@
-const zod = require("zod");
-
-const uploadVideo = zod.object({
-    videoTitle: zod.string().min(1),
-    videoDescription: zod.string(),
-    videoGenre: zod.string(1)
-});
-
-module.exports = { uploadVideo };
